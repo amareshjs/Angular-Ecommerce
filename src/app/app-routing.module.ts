d1b7d9596e807ae48829a9eddad3051658fd8536
amareshjoshi20 <amareshujoshi@gmail.com>
2022-06-24 20:02:12 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListPageComponent } from './templates/list-page/list-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',pathMatch:'full',
+    component:ListPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import{HttpClientModule} from '@angular/common/http';
+import { TemplatesModule } from './templates/templates.module';
+import { ElementsModule } from './elements/elements.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgToastModule } from 'ng-angular-popup';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    TemplatesModule,
+    ElementsModule,
+    NgbModule,
+    NgToastModule
   ],
   providers: [],
   bootstrap: [AppComponent]
